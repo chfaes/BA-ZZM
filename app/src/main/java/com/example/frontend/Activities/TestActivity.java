b6ea7ea17d0e3b9bea1ac9dc1466faadbdc76bbe
chfaes <christoph.faessler@uzh.ch>
2020-03-03 14:02:27 +0100

    Rotating 3D Sphere

--- app/src/main/java/com/example/frontend/Activities/TestActivity.java
+++ app/src/main/java/com/example/frontend/Activities/TestActivity.java
@@ -1,21 +1,36 @@
 package com.example.frontend.Activities;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.ViewGroup;
 
 import com.example.frontend.R;
 
 import org.rajawali3d.loader.IParser;
+import org.rajawali3d.surface.IRajawaliSurface;
+import org.rajawali3d.surface.RajawaliSurfaceView;
 
 
 public class TestActivity extends AppCompatActivity {
+
+    Renderer renderer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
-    }
 
+        final RajawaliSurfaceView surface = new RajawaliSurfaceView(this);
+        surface.setFrameRate(60.0);
+        surface.setRenderMode(IRajawaliSurface.RENDERMODE_WHEN_DIRTY);
+
+        // Add mSurface to your root view
+        addContentView(surface, new ActionBar.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT));
+
+        renderer = new Renderer(this);
+        surface.setSurfaceRenderer(renderer);
+    }
 }
 
 
